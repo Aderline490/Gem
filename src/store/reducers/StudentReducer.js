38e445985aa0555611c0a975f9d6705a808a52ca
Aderline Gashugi <75659434+Aderline490@users.noreply.github.com>
2022-11-16 22:33:14 +0200

    Create StudentReducer.js

---
+++ src/store/reducers/StudentReducer.js
@@ -0,0 +1,10 @@
+import * as actions from '../actions/StudentActions';
+
+export const initialState = {
+    students: [],
+    loading: false,
+  }
+
+export default function StudentsReducer(state = initialState, action){ 
+  return state;
+};
