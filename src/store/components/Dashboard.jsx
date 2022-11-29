5a08d89ed497151f2c3065fd2c4d9645512e009c
Aderline Gashugi <75659434+Aderline490@users.noreply.github.com>
2022-11-29 22:30:52 +0200

    Update Dashboard.jsx

--- src/store/components/Dashboard.jsx
+++ src/store/components/Dashboard.jsx
@@ -1,6 +1,20 @@
+import SideBar from "./SideBar";
+import Header from "./Header";
+
 const Dashboard = () => {
-  return <>
-  </>;
+  return (
+    <>
+      <div className="absolute w-full flex z-0">
+        <SideBar />
+        <div className="w-4/5 flex flex-col">
+          <Header />
+          <div className="">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
