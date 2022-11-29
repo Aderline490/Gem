e9c630f7b76ba8cc69a11010ccba5b5cb028803a
Aderline Gashugi <75659434+Aderline490@users.noreply.github.com>
2022-11-29 22:31:04 +0200

    Create index.jsx

---
+++ src/store/components/index.jsx
@@ -0,0 +1,15 @@
+import SideBar from "./SideBar";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+import Students from "./Students";
+
+const index = () => {
+    return (
+
+        <div className="w-full flex z-0 absolute">
+            
+        </div>
+    );
+};
+
+export default index;
