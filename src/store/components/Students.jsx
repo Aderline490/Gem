0efb961dea4937f2363dc3b9e499cdd9c20b9677
Aderline Gashugi <75659434+Aderline490@users.noreply.github.com>
2022-11-29 22:31:22 +0200

    Update Students.jsx

--- src/store/components/Students.jsx
+++ src/store/components/Students.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import { useState } from "react";
 
 import SideBar from "./SideBar";
 import Header from "./Header";
 import List from "./List";
+import NewStudent from "./NewStudent";
 
 const Students = () => {
+  const [openModal, setOpenModal] = useState(false);
+  
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  }
+
+  const handleCloseModal = () => {
+    console.log("Reacheed");
+    setOpenModal(false);
+    console.log(openModal)
+  }
   return (
-    <div className="flex">
+    <>
+      <div className="absolute w-full flex z-0">
         <SideBar />
         <div className="w-4/5 flex flex-col">
-            <Header />
-            <List />
+          <Header />
+          <List handleOpenModal={handleOpenModal} openModal={openModal}/>
         </div>
-    </div>
+      </div>
+      {openModal && (
+          <NewStudent handleCloseModal={handleCloseModal}/>
+      )}
+    </>
   );
 };
 
