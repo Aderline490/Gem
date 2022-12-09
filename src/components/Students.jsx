a9a51909baa7d2410f4b2ff1b6933bb5e346cb3e
Aderline Gashugi <75659434+Aderline490@users.noreply.github.com>
2022-12-09 22:17:47 +0200

    Update Students.jsx

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,33 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import SideBar from "./SideBar";
 import Header from "./Header";
 import List from "./List";
 import NewStudent from "./NewStudent";
 
+import { fetchStudents } from "../store/actions/StudentActions";
+
 const Students = () => {
+  const [student, setStudent] = useState({});
   const [openModal, setOpenModal] = useState(false);
-  
-  const handleOpenModal = () => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleOpenModal = (student, isEdit) => {
     setOpenModal(true);
-  }
+    setStudent(student);
+    setIsEdit(isEdit);
+    console.log(student);
+  };
 
   const handleCloseModal = () => {
-    console.log("Reacheed");
     setOpenModal(false);
-    console.log(openModal)
-  }
+  };
   return (
     <>
       <div className="absolute w-full flex z-0">
         <SideBar />
         <div className="w-4/5 flex flex-col">
           <Header />
-          <List handleOpenModal={handleOpenModal} openModal={openModal}/>
+          <List handleOpenModal={handleOpenModal} openModal={openModal} />
         </div>
       </div>
       {openModal && (
-          <NewStudent handleCloseModal={handleCloseModal}/>
+        <NewStudent
+          isEdit={isEdit}
+          student={student}
+          handleCloseModal={handleCloseModal}
+        />
       )}
     </>
   );
