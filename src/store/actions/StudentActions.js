55311a3e91a26c85fb357bea7f8312cc1304e6ad
Aderline Gashugi <75659434+Aderline490@users.noreply.github.com>
2022-12-09 22:17:50 +0200

    Update StudentActions.js

--- src/store/actions/StudentActions.js
+++ src/store/actions/StudentActions.js
@@ -4,23 +4,41 @@ const url = "http://localhost:8000/students";
 
 export async function fetchStudents() {
   try {
+    console.log("fetching....")
     const response = await axios.get(`${url}/all`, { headers: {} });
     return response.data.data;
   } catch (error) {
-    console.log(error);
     return error?.message;
   }
 }
 
 export async function createStudent(student) {
     try {
-        console.log(student);
-        const response = await axios.post(`${url}`, student, { headers: {} });
-        console.log(response);
-        return response;
+      const response = await axios.post(`${url}`, student, { headers: {} });
+      window.location.reload(false);
+      return response.data.success;
+    } catch (error) {
+      return error?.message;
     }
-    catch(error) {
-        console.log(error);
-        return error?.message;
+}
+
+export async function updateStudent(student) {
+  try {
+    const response = await axios.put(`${url}/${student.id}`, student, {headers: {}});
+    window.location.reload(false);
+    return response.data.success;
+  }
+  catch(error) {
+    return error?.message;
+  }
+}
+
+export async function deleteStudent(id) {
+    try {
+      const response = await axios.delete(`${url}/${id}`, { headers: {} });
+      window.location.reload(false);
+      return response.data.success;
+    } catch (error) {
+      return error?.message;
     }
 }
